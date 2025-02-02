513e6ecb1b65f9953712d1d0df897cb214da124d
Saleh Hamed <salehhamed773@gmail.com>
2025-02-02 17:37:02 +0200

    edit-task-7

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,4 @@
+# ["INSERT INTO customer", "124 Happiness  Ave."]
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
