80139b5480f355cc31253f4c3fcaf2032ae7781f
Saleh Hamed <salehhamed773@gmail.com>
2025-02-02 17:30:49 +0200

    task-5

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+SELECT 
+    COLUMN_NAME,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND 
+    TABLE_NAME = 'Books';
